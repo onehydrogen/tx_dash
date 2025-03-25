77980d9ac6b6c3d3b3175987754f766a46f31eba
onehydrogenatom <111794401+onehydrogenatom@users.noreply.github.com>
2025-03-25 14:28:07 -0500

    Fix dependency conflict between Flask and Werkzeug

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return 'Hello World! Flask is now working!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Bind to PORT if defined, otherwise default to 5000
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
